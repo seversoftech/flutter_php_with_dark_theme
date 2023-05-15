ec26ded25a6a2f165efbdd649a972a907114faa7
Ibeabuchi Emmanuel I. (Sever) <developersever@gmail.com>
2023-05-15 07:15:21 +0100

    Update config.php

--- config.php
+++ config.php
@@ -7,6 +7,7 @@ $dbname="rem";
 $conn=new mysqli($servername,$username,$password,$dbname);
 
 if($conn->connect_error){
+	// Connection Error;
 	die("Connection Failed".$conn->connect_error);
 	return;
 }else{
